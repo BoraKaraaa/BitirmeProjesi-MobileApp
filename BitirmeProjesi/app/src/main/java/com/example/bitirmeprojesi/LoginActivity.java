74990b15e71c73cc4f8a71720625aa54f1a1baee
Bora Kara <kborakaraa@gmail.com>
2024-01-15 03:29:52 +0300

    SignUp + SignIn with GMAIL implementation

--- BitirmeProjesi/app/src/main/java/com/example/bitirmeprojesi/LoginActivity.java
+++ BitirmeProjesi/app/src/main/java/com/example/bitirmeprojesi/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.bitirmeprojesi;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +25,15 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -30,10 +47,15 @@ public class LoginActivity extends AppCompatActivity
 
     EditText editTextEmail, editTextPassword;
     Button buttonSignIn;
-    ImageView imageGoogle, imageFacebook, imagePasswordToggle;
+    ImageView imageGoogle, imagePasswordToggle;
     ProgressBar progressBar;
     TextView textViewSignUp, textForgetPassword;
     FirebaseAuth mAuth;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signUpRequest;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -50,6 +72,7 @@ public class LoginActivity extends AppCompatActivity
 
         setForgetPasswordTextListener();
         setLogInTextListener();
+        setLogInGoogleImageListener();
         setSignUpButtonListener();
     }
 
@@ -61,7 +84,6 @@ public class LoginActivity extends AppCompatActivity
         buttonSignIn = findViewById(R.id.sign_in_button);
 
         imageGoogle = findViewById(R.id.google_button);
-        imageFacebook = findViewById(R.id.facebook_button);
 
         imagePasswordToggle = findViewById(R.id.password_toggle);
 
@@ -121,6 +143,20 @@ public class LoginActivity extends AppCompatActivity
         throw new RegisterException(ERegistrationError.INVALID_PASSWORD);
     }
 
+    private void setForgetPasswordTextListener()
+    {
+        textForgetPassword.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getApplicationContext(), ForgetPasswordActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
     private void setLogInTextListener()
     {
         textViewSignUp.setOnClickListener(new View.OnClickListener()
@@ -135,19 +171,80 @@ public class LoginActivity extends AppCompatActivity
         });
     }
 
-    private void setForgetPasswordTextListener()
+    private void setLogInGoogleImageListener()
     {
-        textForgetPassword.setOnClickListener(new View.OnClickListener()
+        oneTapClient = Identity.getSignInClient(this);
+        signUpRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.web_client_id))
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build();
+
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
+                new ActivityResultCallback<ActivityResult>()
+                {
+                    @Override
+                    public void onActivityResult(ActivityResult result)
+                    {
+                        if(result.getResultCode() == Activity.RESULT_OK)
+                        {
+                            try
+                            {
+                                SignInCredential credential = oneTapClient
+                                        .getSignInCredentialFromIntent(result.getData());
+
+                                String idToken = credential.getGoogleIdToken();
+
+                                if (idToken !=  null)
+                                {
+                                    String email = credential.getId();
+                                    Toast.makeText(getApplicationContext(),
+                                            "Email: " + email, Toast.LENGTH_SHORT).show();
+                                }
+                            } catch (ApiException e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+
+        imageGoogle.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(getApplicationContext(), ForgetPasswordActivity.class);
-                startActivity(intent);
-                finish();
+                oneTapClient.beginSignIn(signUpRequest)
+                    .addOnSuccessListener(LoginActivity.this, new OnSuccessListener<BeginSignInResult>()
+                    {
+                        @Override
+                        public void onSuccess(BeginSignInResult result)
+                        {
+                            IntentSenderRequest intentSenderRequest
+                                    = new IntentSenderRequest.Builder(
+                                    result.getPendingIntent().getIntentSender()).build();
+
+                            activityResultLauncher.launch(intentSenderRequest);
+                        }
+                    })
+                    .addOnFailureListener(LoginActivity.this, new OnFailureListener()
+                    {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+                            // No Google Accounts found. Just continue presenting the signed-out UI.
+                            Log.d(TAG, e.getLocalizedMessage());
+                        }
+                    });
             }
         });
     }
+
     private void setSignUpButtonListener()
     {
         buttonSignIn.setOnClickListener(new View.OnClickListener()
--- BitirmeProjesi/app/src/main/java/com/example/bitirmeprojesi/SignupActivity.java
+++ BitirmeProjesi/app/src/main/java/com/example/bitirmeprojesi/SignupActivity.java
@@ -1,13 +1,23 @@
 package com.example.bitirmeprojesi;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +26,15 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -31,10 +49,15 @@ public class SignupActivity extends AppCompatActivity
 
     EditText editTextUserName, editTextEmail, editTextPassword, editTextConfirmPassword;
     Button buttonSignUp;
-    ImageView imageGoogle, imageFacebook, imagePasswordToggle, imageConfirmPasswordToggle;
+    ImageView imageGoogle, imagePasswordToggle, imageConfirmPasswordToggle;
     ProgressBar progressBar;
     TextView textViewLogIn;
     FirebaseAuth mAuth;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signUpRequest;
+    private static final int REQ_ONE_TAP = 2;  // Can be any integer unique to the Activity.
+    private boolean showOneTapUI = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -50,6 +73,7 @@ public class SignupActivity extends AppCompatActivity
         clickablePasswordEye();
 
         setLogInTextListener();
+        setLogInGoogleImageListener();
         setSignUpButtonListener();
     }
 
@@ -63,7 +87,6 @@ public class SignupActivity extends AppCompatActivity
         buttonSignUp = findViewById(R.id.sign_up_button);
 
         imageGoogle = findViewById(R.id.google_button);
-        imageFacebook = findViewById(R.id.facebook_button);
 
         imagePasswordToggle = findViewById(R.id.password_toggle);
         imageConfirmPasswordToggle = findViewById(R.id.confirm_password_toggle);
@@ -166,6 +189,80 @@ public class SignupActivity extends AppCompatActivity
         });
     }
 
+    private void setLogInGoogleImageListener()
+    {
+        oneTapClient = Identity.getSignInClient(this);
+        signUpRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.web_client_id))
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build();
+
+        ActivityResultLauncher<IntentSenderRequest> activityResultLauncher =
+                registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(),
+            new ActivityResultCallback<ActivityResult>()
+            {
+                @Override
+                public void onActivityResult(ActivityResult result)
+                {
+                    if(result.getResultCode() == Activity.RESULT_OK)
+                    {
+                        try
+                        {
+                            SignInCredential credential = oneTapClient
+                                    .getSignInCredentialFromIntent(result.getData());
+
+                            String idToken = credential.getGoogleIdToken();
+
+                            if (idToken !=  null)
+                            {
+                                String email = credential.getId();
+                                Toast.makeText(getApplicationContext(),
+                                        "Email: " + email, Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (ApiException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+
+        imageGoogle.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                oneTapClient.beginSignIn(signUpRequest)
+                    .addOnSuccessListener(SignupActivity.this, new OnSuccessListener<BeginSignInResult>()
+                    {
+                        @Override
+                        public void onSuccess(BeginSignInResult result)
+                        {
+                            IntentSenderRequest intentSenderRequest
+                                = new IntentSenderRequest.Builder(
+                                        result.getPendingIntent().getIntentSender()).build();
+
+                            activityResultLauncher.launch(intentSenderRequest);
+                        }
+                    })
+                    .addOnFailureListener(SignupActivity.this, new OnFailureListener()
+                    {
+                        @Override
+                        public void onFailure(@NonNull Exception e)
+                        {
+                            // No Google Accounts found. Just continue presenting the signed-out UI.
+                            Log.d(TAG, e.getLocalizedMessage());
+                        }
+                    });
+            }
+        });
+    }
+
     private void setSignUpButtonListener()
     {
         buttonSignUp.setOnClickListener(new View.OnClickListener()
@@ -199,30 +296,29 @@ public class SignupActivity extends AppCompatActivity
                     }
 
                     mAuth.createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                        {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task)
                             {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task)
+                                progressBar.setVisibility(View.GONE);
+
+                                if (task.isSuccessful())
                                 {
-                                    progressBar.setVisibility(View.GONE);
+                                    Toast.makeText(SignupActivity.this, "Account Created.",
+                                            Toast.LENGTH_SHORT).show();
 
-                                    if (task.isSuccessful())
-                                    {
-                                        Toast.makeText(SignupActivity.this, "Account Created.",
-                                                Toast.LENGTH_SHORT).show();
-
-                                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                        startActivity(intent);
-                                        finish();
-                                    }
-                                    else
-                                    {
-                                        Toast.makeText(SignupActivity.this, "Authentication failed.",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
                                 }
-                            });
-
+                                else
+                                {
+                                    Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                 }
                 catch (RegisterException e)
                 {
